bace23e0946ab2110bf418e41d800708962340d3
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2025-08-17 15:53:36 +0200

    Update build.rs

--- speexdsp-sys/build.rs
+++ speexdsp-sys/build.rs
@@ -1,7 +1,7 @@
+use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::env;
 
 /// Tries to use system speexdsp and emits necessary build script instructions.
 fn try_system_speexdsp() -> Result<pkg_config::Library, pkg_config::Error> {
@@ -24,20 +24,6 @@ fn try_system_speexdsp() -> Result<pkg_config::Library, pkg_config::Error> {
 fn main() {
     let vendored = env::var("CARGO_FEATURE_VENDORED").is_ok();
 
-    let common_c_files = [
-        "buffer.c",
-        "fftwrap.c",
-        "filterbank.c",
-        "jitter.c",
-        "kiss_fft.c",
-        "kiss_fftr.c",
-        "mdf.c",
-        "preprocess.c",
-        "resample.c",
-        "scal.c",
-        "smallft.c",
-    ];
-
     let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     let mut include_paths: Vec<PathBuf> = Vec::new();
@@ -51,6 +37,20 @@ fn main() {
         include_paths.push(PathBuf::from("speexdsp/libspeexdsp"));
 
         let mut cfg = cc::Build::new();
+
+        let common_c_files = [
+            "buffer.c",
+            "fftwrap.c",
+            "filterbank.c",
+            "jitter.c",
+            "kiss_fft.c",
+            "kiss_fftr.c",
+            "mdf.c",
+            "preprocess.c",
+            "resample.c",
+            "scal.c",
+            "smallft.c",
+        ];
 
         for f in common_c_files {
             cfg.file(Path::new("speexdsp/libspeexdsp").join(f));
